8efce2e485ea3325e1cb6c1e4694070f6bfec277
H-hidetaka <sansanhidetaka@gmail.com>
2021-08-05 17:23:31 +0900

    [Modify]メソッド追加

--- front/components/Typing.vue
+++ front/components/Typing.vue
@@ -1,76 +1,69 @@
 <template>
-  <div id="app">
+<div id="app">
     <div v-if="playing">
-      <span>{{ pressed }}</span>{{ word }}
-      <br>
-      <br>
-      miss:{{ miss }}
+    <span>{{ pressed }}</span>{{ word }}
+    <br>
+    <br>
+    miss:{{ miss }}
     </div>
     <div v-else>Spaceでスタート</div>
-  </div>
+    <div>
+        {{ push }}
+    <div>
+</div>
 </template>
 
 <script lang="ts">
-import Vue, { PropOptions } from 'vue';
+import Vue from 'vue';
+// import Type from 'type';
 
+const OnGUI:string = 'inputed';
 export default Vue.extend({
-    data() {
-        return {
-
-            words: ['apple', 'banana', 'grape', 'aiueo'],
-            word: '',
-            pressed: '',
-            miss: 0,
-            playing: false,
-        };
+data() {
+    return {
+    words: ['apple', 'banana', 'grape'],
+    word: '',
+    pressed: '',
+    miss: 0,
+    playing: false,
+    };
+},
+created() {
+    addEventListener('keydown', (e) => {
+    if (e.key !== ' ' || this.playing) {
+        return;
+    }
+    this.playing = true;
+    this.setWord();
+    this.keyDown();
+    this.keyBoard();
+});
+},
+methods: {
+    setWord() {
+      this.word = this.words.splice(Math.floor(Math.random() * this.words.length), 1)[0];
     },
-    created() {
-        addEventListener('keydown', (e) => {
-            if (e.key !== ' ' || this.playing) {
-                return;
-            }
-            this.playing = true;
-            this.setWord();
-            this.keyDown();
-        });
+    keyDown() {
+    addEventListener('keydown', (e) => {
+        if (e.key !== this.word[0]) {
+        this.miss++;
+        return;
+        }
+        this.pressed += e.key;
+        this.word = this.word.slice(1);
+        if (this.word.length === 0) {
+        this.pressed = '';
+        if (this.words.length === 0) {
+            this.word = 'おしまい';
+            return;
+        }
+        this.setWord();
+        }
+    });
     },
-    methods: {
-        setWord() {
-            this.word = this.words.splice(Math.floor(Math.random() * this.words.length), 1)[0];
-        },
-        keyDown() {
-            addEventListener('keydown', (e) => {
-                if (e.key !== this.word[0]) {
-                    this.miss++;
-                    return;
-                }
-                this.pressed += e.key;
-                this.word = this.word.slice(1);
-                if (this.word.length === 0) {
-                    this.pressed = '';
-                    if (this.words.length === 0) {
-                        this.word = 'おしまい';
-                        return;
-                    }
-                    this.setWord();
-                }
-            });
-        },
-    },
+    keyBoard() {
+        this.push = this.push.
+    }
+},
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-span {
-  opacity: 0.5;
-}
-</style>
---
+++ front/types/type.d.ts
@@ -0,0 +1,9 @@
+export interface TypeData {
+  type: {
+    name: string
+    color: string
+    keydown: string
+    OnGUI: string
+    InputKey: string
+  }
+}
