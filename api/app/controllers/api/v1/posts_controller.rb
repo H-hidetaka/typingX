c937a10bad78adb2b7fc8d3cf4ee9312e35a0a50
H-hidetaka <sansanhidetaka@gmail.com>
2021-09-11 22:50:32 +0900

    [Modify]postsコントローラー変更

--- api/app/controllers/api/v1/posts_controller.rb
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -2,11 +2,16 @@ module Api
   module V1
     class Api::V1::PostsController < ApplicationController
       def index
-        render json:
+        render json: cards =
+        {
+          title: '1',
+          to:'/gamelists/retro_game_introduction',
+          body:'No.1 チュートリアル'
+          }
           {
-            title: 'ちくわ',
-            src:'/various_game.jpg',
-            to:'/gamelists/retro_game_introduction'
+            title: '2',
+            to:'/gamelists/playable_game_collection/play_game',
+            body:'No.2 チュートリアル'
           }
       end
     end
