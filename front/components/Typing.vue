377448698c997adb68a17ebd2ba3489d14521c47
H-hidetaka <sansanhidetaka@gmail.com>
2021-11-10 16:39:22 +0900

    [Retry]1、画面に文字を表示

--- front/components/Typing.vue
+++ front/components/Typing.vue
@@ -1,42 +1,40 @@
 <template>
+  <v-container fluid>
+    <v-row>
+      <v-col auto>
   <div>
-    <template v-if="!count">
-      <figure>
-        <img class="w-full" src="@/assets/game-image.jpg" alt="ウマ娘プリティーダービー">
-      </figure>
-      <div class="mt-8 text-center">
-        <button type="button" class="font-bold app-btn" @click="countDown">タイピングを始める</button>
-      </div>
-    </template>
-    <p class="mt-4 text-center text-red-400 text-9xl font-bold" v-else>
-      {{ count }}
-    </p>
+    <h6 class=text-center
+      cols="12"
+    >
+    {{title}}
+    </h6>
+  <!-- １、文字を表示する
+  ２、文字がキーボードに反応する処理を書く
+  ３、文字を打つと、色が変わる処理を書く
+  ４、３画面表示
+  ５、railsからデータを取得できるようにするaxios使用
+  ６、スコア機能を作成し、決められた文字数を打ち込んだ時に画面を切りかえる（v-if使用)
+  ７、スコアはタイピング速度と精度で判定 -->
+
   </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject, ref } from 'vue'
-import TypingKey from '@/composables/use-typing-key'
-import { TypingStore } from '@/utils/useTyping'
-
-export default defineComponent({
-  setup () {
-    const { phase } = inject(TypingKey) as TypingStore
-    const count = ref<number>(0)
-    const countDown = () => {
-      count.value = 3
-      const countInterval = setInterval(() => {
-        count.value--
-        if (!count.value) {
-          clearInterval(countInterval)
-          phase.value = 'START'
-        }
-      }, 1000)
-    }
-    return {
-      count,
-      countDown
-    }
+import Vue from 'vue';
+  interface Human {
+    name: string;
+    age: number;
   }
-})
+
+export default Vue.extend({
+  data:() => {
+        return {
+            title: 'タイピングを楽しく学ぶ！',
+            message: 'タイピングが苦手な方や、ちょっとした暇を潰したい方に送るタインピング型webサービス',
+        };
+    },
+});
 </script>
